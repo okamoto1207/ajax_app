bd17f1f60b83ddd31b124980ffe782c9e6b30258
okamoto1207 <appletoxicspark12.7@gmail.com>
2021-11-04 19:13:39 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.order(id: "DESC")
-  end
+ def index
+   @posts = Post.order(id: "DESC")
+ end
 
+  # コメントアウトする
   # def new
   # end
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index 
-  end
+ def create
+   Post.create(content: params[:content])
+   redirect_to action: :index  # 追記する
+ end
+
 end
